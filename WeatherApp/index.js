252b7971f9a81ed693522f25b13b46a4ec8b4f02
Pankaj Das <126493776+kumardas67@users.noreply.github.com>
2023-09-19 04:28:32 +0000

    Added the index.js and style.css

---
+++ WeatherApp/index.js
@@ -0,0 +1,56 @@
+const apiKey = 'a1dbb88ecba7fe3ca52b18f4c810ace9'
+
+
+const weatherDataEl = document.getElementById('weather-data');
+const cityInputEl = document.getElementById('city-input');
+const formEl = document.querySelector('form')
+
+formEl.addEventListener('submit',(event)=>{
+    event.preventDefault()
+
+    const cityValue = cityInputEl.value;
+    getWeatherData(cityValue);
+});
+
+async function getWeatherData(cityValue){
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}&units=metric`)
+
+        if(!response.ok){
+            throw new Error('Network response was not ok')
+        }
+
+        const data = await response.json()
+        // console.log(data);
+
+        const temperature = Math.round(data.main.temp)
+        const description = data.weather[0].description
+        const icon = data.weather[0].icon
+        const details = [
+            `Feels like: ${Math.round(data.main.feels_like)}`,
+            `Humidity: ${data.main.humidity}%`,
+            `Wind Speed: ${data.wind.speed}`,
+        ]
+
+        weatherDataEl.querySelector(
+            '.icon'
+            ).innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="Weather-icon">`
+
+            weatherDataEl.querySelector(
+                '.temperature'
+                ).textContent = `${temperature}°C`;
+
+            weatherDataEl.querySelector(
+                '.description'
+                ).textContent = description;
+
+            weatherDataEl.querySelector(
+                '.details'
+                ).innerHTML = details.map((detail)=>{
+                    `<div>${detail}</div>`
+                }).join('');
+    } catch (error) {
+        
+    }
+}
+
